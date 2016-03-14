32aa48982e2a9e179802da9c717b99c5ba3e948a
Andrew Stephens <andrewmediaprod@gmail.com>
2016-03-13 23:19:49 -0400

    Updated Main Activity and Loaded OpenCV

--- app/src/main/java/edu/tufts/cs/thejigisup/MainActivity.java
+++ app/src/main/java/edu/tufts/cs/thejigisup/MainActivity.java
@@ -2,12 +2,42 @@ package edu.tufts.cs.thejigisup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TheJigIsUp::Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+    }
+
 }
