e66a4c8550586e9d43874fb89f0b29ac32f3e958
Andrew Stephens <andrewmediaprod@gmail.com>
2016-03-15 16:46:46 -0400

    Fixed OpenCV Static Initialization

--- app/src/main/java/edu/tufts/cs/thejigisup/MainActivity.java
+++ app/src/main/java/edu/tufts/cs/thejigisup/MainActivity.java
@@ -4,40 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "TheJigIsUp::Main";
 
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV Initialization Failed");
+        } else {
+            Log.i(TAG, "OpenCV Initialization Success");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
     }
 
 }
