ccbce8a139b68a680c88e9ed09d9a9db342641cd
Ben Lewin <blewin1@gmail.com>
2016-04-21 04:04:11 -0400

    box gallery now exists!

---
+++ app/src/main/java/edu/tufts/cs/thejigisup/BoxGalleryActivity.java
@@ -0,0 +1,13 @@
+package edu.tufts.cs.thejigisup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoxGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_box_gallery);
+    }
+}
