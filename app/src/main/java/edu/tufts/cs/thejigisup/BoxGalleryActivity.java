66c4cfb3ef75db5481da7fec3282eb933c6668d0
Ben Lewin <blewin1@gmail.com>
2016-04-21 17:34:29 -0400

    box gallery fixes, still not working

--- app/src/main/java/edu/tufts/cs/thejigisup/BoxGalleryActivity.java
+++ app/src/main/java/edu/tufts/cs/thejigisup/BoxGalleryActivity.java
@@ -30,13 +30,20 @@ import java.util.Collections;
 @SuppressWarnings("deprecation")
 public class BoxGalleryActivity extends Activity {
     String currentBoxPath;
+    String currentBoxPathSmall;
 
-    private String[] getPictureList() {
-        String path = Environment.getExternalStorageDirectory().toString() + "/jig/box";
+    private String[] getPictureList(boolean isSmall) {
+        String path;
+        if(isSmall) {
+            path = Environment.getExternalStorageDirectory().toString() + "/jig/box/small";
+        } else {
+            path = Environment.getExternalStorageDirectory().toString() + "/jig/box";
+        }
         Log.d("Files","Path: "+path);
         File f = new File(path);
+        f.mkdirs();
         File images[] = f.listFiles();
-        if (images.length == 0) {
+        if (images == null || images.length == 0) {
             return null;
         }
         Log.d("Files","Size: "+images.length);
@@ -55,6 +62,7 @@ public class BoxGalleryActivity extends Activity {
 
     //the images to display
     String[] imageIDs;
+    String[] smallImageIDs;
 
 
     @Override
@@ -64,7 +72,8 @@ public class BoxGalleryActivity extends Activity {
 
         final Activity baseContext = this;
 
-        imageIDs = getPictureList();
+        imageIDs = getPictureList(false);
+        smallImageIDs = getPictureList(true);
 
         if (imageIDs == null) {
             Log.d("jig::BoxGallery","No images, going to box taking picture");
@@ -98,6 +107,7 @@ public class BoxGalleryActivity extends Activity {
 
                 Log.d("jig::boxGallery", "image view was long clicked.");
                 currentBoxPath = imageIDs[position];
+                currentBoxPathSmall = smallImageIDs[position];
                 DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -106,6 +116,8 @@ public class BoxGalleryActivity extends Activity {
                                 //Yes button clicked
                                 File toDelete = new File(currentBoxPath);
                                 boolean isDeleted = toDelete.delete();
+                                toDelete = new File(currentBoxPathSmall);
+                                isDeleted = toDelete.delete();
                                 Intent intent;
                                 intent = new Intent(getBaseContext(), BoxGalleryActivity.class);
                                 startActivity(intent);
@@ -154,47 +166,11 @@ public class BoxGalleryActivity extends Activity {
         // returns an ImageView view
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView = new ImageView(context);
-            imageView.setImageBitmap(decodeSampledBitmapFromFile(new File(imageIDs[position]).toString(), 200, 200));
-            imageView.setImageURI(Uri.parse(new File(imageIDs[position]).toString()));
+            imageView.setImageURI(Uri.parse(new File(smallImageIDs[position]).toString()));
             imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             imageView.setBackgroundResource(itemBackground);
             return imageView;
         }
     }
-    public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
 
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-    public static Bitmap decodeSampledBitmapFromFile(String path,int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(path, options);
-    }
 }
--- app/src/main/java/edu/tufts/cs/thejigisup/PhotoActivity.java
+++ app/src/main/java/edu/tufts/cs/thejigisup/PhotoActivity.java
@@ -6,6 +6,7 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -180,34 +181,50 @@ public class PhotoActivity extends Activity implements CvCameraViewListener2, On
         Mat mBgr = new Mat();
         Imgproc.cvtColor(mRgba, mBgr, Imgproc.COLOR_RGBA2BGR, 3);
 
+        Mat mBgrMini = new Mat();
+        Size sz = new Size(200,200);
+        Imgproc.resize( mBgr, mBgrMini, sz );
+
         long saveTime = System.currentTimeMillis();
 
         // Get Path Directories, Creating Folders if Necessary
         File sd = Environment.getExternalStorageDirectory();
         File pieceDirectory = new File(sd, "jig/pieces/");
         File boxDirectory = new File(sd, "jig/box/");
+        File smallBoxDirectory = new File(sd, "jig/box/small/");
         pieceDirectory.mkdirs();
         boxDirectory.mkdirs();
+        smallBoxDirectory.mkdirs();
 
         // Save in Separate Folder Based on Mode
         String imageFilename = saveTime + ".png";
         File imageFile;
+        File smallImageFile = null;
         if (mode == ActivityMode.PUZZLE_PIECES) {
             imageFile = new File(pieceDirectory, imageFilename);
         } else {
             imageFile = new File(boxDirectory, imageFilename);
+            smallImageFile = new File(smallBoxDirectory, imageFilename);
         }
 
         imageFilename = imageFile.getAbsolutePath();
         Log.i(TAG, "Saving Image: " + imageFilename);
         Boolean imageSaved = Imgcodecs.imwrite(imageFilename, mBgr);
-
         if (imageSaved) {
             Log.i(TAG, "SUCCESS writing image to external storage");
         } else {
             Log.i(TAG, "Fail writing image to external storage");
         }
+        if(mode == ActivityMode.PUZZLE_BOX) {
+            String smallFilePath = smallImageFile.getAbsolutePath();
+            Boolean smallImageSaved = Imgcodecs.imwrite(smallFilePath, mBgrMini);
 
+            if (smallImageSaved) {
+                Log.i(TAG, "SUCCESS writing small image to external storage");
+            } else {
+                Log.i(TAG, "Fail writing smallimage to external storage");
+            }
+        }
         return imageFilename;
 
     }
